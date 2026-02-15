2f5bd8d9ad42c8a1fba9d451dd1ea6e6e201099c
Pinak Ranjan Sahoo <pinak_hits@hotmail.com>
2026-02-15 08:42:57 +0530

    Removed unnecessory files

--- lib/providers/push_notice_navigation_provider.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-/// When set, the Notices tab should open the relevant notice (by `newsId`).
-final pendingNoticeOpenIdProvider = StateProvider<int?>((ref) => null);
